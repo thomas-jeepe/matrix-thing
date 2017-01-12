6134458fdffbeb567064aef35e0492f0495af4e6
penguinsoccer <penguinsoccer@outlook.com>
2017-01-12 14:30:36 -0500

    able to do rref and get from points

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import * as _ from 'lodash'
+
 export class Quadratic {
   constructor(public equation: [number, number, number]) {}
 
@@ -90,6 +92,49 @@ export class Quadratic {
     let [a, b, c] = this.equation
     return b * b - 4 * a * c
   }
+
+  solveWith(x: number) {
+    let [a, b, c] = this.equation
+    return a * (x*x) + b * x + c
+  }
+
+  /**
+   * Gets discriminant if y is set to a special number
+   */
+  specialDiscriminant(y: number) {
+    let [a, b, c] = this.equation
+    const newC = c - y
+    return b * b - 4 * a * newC
+  }
+
+  /**
+   * If the equation has a real solution for y
+   */
+  contains(y: number) {
+    return this.specialDiscriminant(y) >= 0
+  }
+
+  solveFor(y: number) {
+    let [a, b, c] = this.equation
+    return [
+      (-b + Math.sqrt(this.specialDiscriminant(y))) / (2 * a),
+      (-b - Math.sqrt(this.specialDiscriminant(y))) / (2 * a)
+    ]
+  }
+
+  toString() {
+    let [a, b, c] = this.equation
+    return `y = ${a}x² + ${b}x + ${c}`
+  }
+
+  static fromPoints([ax, ay]: [number, number], [bx, by]: [number, number], [cx, cy]: [number, number]) {
+    const answer = rref([
+      [ax * ax, ax, 1, ay],
+      [bx * bx, bx, 1, by],
+      [cx * cx, cx, 1, cy]
+    ])
+    return new Quadratic([answer[0][3], answer[1][3], answer[2][3]])
+  }
 }
 
 export function groupByNumber(arr: number[]) {
@@ -113,14 +158,53 @@ export function primeFactors(n: number) {
   return factors
 }
 
-const result = new Quadratic([1, -14, 58])
+export class Polynomial {
+  static square(a: number, b: number) {
+    if (a < 0) {
+      return `-${a*a}x² - ${b * a + b * a}x - ${b * b}`
+    }
+    return `${a*a}x² + ${b * a + b * a}x + ${b * b}`
+  }
+}
 
-console.log(result.yIntercept())
-console.log(result.symmetricPoint())
-console.log(result.xIntercept())
-console.log(result.vertex())
-console.log(result.focus())
-console.log(result.solve())
-console.log(result.isReal())
-console.log(result.imaginary())
-console.log(result.discriminant())
+export function rref(matrix: number[][]) {
+  const columns = matrix[0].length
+  for (let i = 0; i < columns; i++) {
+    for (let j = i; j < matrix.length; j++) {
+      if (j === i) {
+        matrix[i] = matrix[i].map(v => v / matrix[i][j])
+      } else {
+        const refMatrix = matrix[i]
+        matrix[j] = matrix[j].map((v, k) => v - refMatrix[k] * matrix[j][i])
+      }
+    }
+  }
+  for (let i = columns - 2; i >= 0; i--) {
+    for (let j = i - 1; j >= 0; j--) {
+      const refMatrix = matrix[i]
+      matrix[j] = matrix[j].map((v, k) => v - refMatrix[k] * matrix[j][i])
+    }
+  }
+  return matrix
+}
+
+const result = Quadratic.fromPoints(
+  [1, 6],
+  [3, 26],
+  [-2, 21]
+)
+// const result = new Quadratic([1, -6, 2])
+console.log(result.toString())
+// console.log(result.yIntercept())
+// console.log(result.symmetricPoint())
+// console.log(result.xIntercept())
+// console.log(result.vertex())
+// console.log(result.focus())
+// console.log(result.solve())
+// console.log(result.isReal())
+// console.log(result.imaginary())
+// console.log(result.discriminant())
+// console.log(result.solveWith(-9))
+// console.log(result.solveFor(15))
+// console.log(result.contains(0))
+// console.log(Polynomial.square(1, -7))
